1f66f71e1c9887941cb41b3742e6dd74d652f1d2
Ryan Allen <rallenwasd@gmail.com>
2019-12-02 21:43:09 -0700

    Refactor seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,45 +5,64 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Shelter.create(name: "Ridiculous Test Name",
-               address: "124 Fake Ln.",
-               city: "Faketown",
-               state: "FK",
-               zip: "55555")
-Shelter.create(name: "Even Worse Test Name",
-               address: "45 Dumpster Fire Alley",
-               city: "Faketown",
-               state: "FK",
-               zip: "55555")
-Shelter.find(1).pets.create({
-                               name: "Bill",
-                               approx_age: 3,
-                               sex: "male",
-                               image_path: "https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_1280.jpg",
-                               description: "Very canine",
-                               adoptable: true
-                             })
-Shelter.find(1).pets.create({
-                               name: "Phil",
-                               approx_age: 1,
-                               sex: "male",
-                               image_path: "https://boygeniusreport.files.wordpress.com/2016/11/puppy-dog.jpg",
-                               description: "Very canine",
-                               adoptable: true
-                             })
-Shelter.find(2).pets.create({
-                               name: "Jill",
-                               approx_age: 5,
-                               sex: "female",
-                               image_path: "http://files.alfresco.mjh.group/alfresco_images/DVM360//2019/02/27/c23624de-1b4c-4e61-961f-db10dcf27341/veterinary-black-puppy-licking-wood-AdobeStock_211167675-450.jpg",
-                               description: "Very canine",
-                               adoptable: true
-                             })
-Shelter.find(2).pets.create({
-                               name: "Will",
-                               approx_age: 2,
-                               sex: "male",
-                               image_path: "https://image.cnbcfm.com/api/v1/image/105992231-1561667465295gettyimages-521697453.jpeg",
-                               description: "Very canine",
-                               adoptable: true
-                             })
+Pet.destroy_all
+Review.destroy_all
+Shelter.destroy_all
+
+shelter_1 = Shelter.create(
+  name: "Ridiculous Test Name",
+  address: "124 Fake Ln.",
+  city: "Faketown",
+  state: "FK",
+  zip: "55555")
+
+shelter_2 = Shelter.create(
+  name: "Even Worse Test Name",
+  address: "45 Dumpster Fire Alley",
+  city: "Faketown",
+  state: "FK",
+  zip: "55555")
+
+shelter_1.pets.create({
+  name: "Bill",
+  approx_age: 3,
+  sex: "male",
+  image_path: "https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_1280.jpg",
+  description: "Very canine",
+  adoptable: true})
+
+shelter_1.pets.create({
+  name: "Phil",
+  approx_age: 1,
+  sex: "male",
+  image_path: "https://boygeniusreport.files.wordpress.com/2016/11/puppy-dog.jpg",
+  description: "Very canine",
+  adoptable: true})
+
+shelter_2.pets.create({
+  name: "Jill",
+  approx_age: 5,
+  sex: "female",
+  image_path: "http://files.alfresco.mjh.group/alfresco_images/DVM360//2019/02/27/c23624de-1b4c-4e61-961f-db10dcf27341/veterinary-black-puppy-licking-wood-AdobeStock_211167675-450.jpg",
+  description: "Very canine",
+  adoptable: true})
+
+shelter_2.pets.create({
+  name: "Will",
+  approx_age: 2,
+  sex: "male",
+  image_path: "https://image.cnbcfm.com/api/v1/image/105992231-1561667465295gettyimages-521697453.jpeg",
+  description: "Very canine",
+  adoptable: true})
+
+shelter_1.reviews.create!(
+ title: "Great Location",
+ rating: 4,
+ content: "3n22 ewlrjwe kej wrkjw nwke nqln qn onrkew 3k iw qm",
+ image_path: "https://www.shutterstock.com/image-vector/location-pin-700686334")
+
+shelter_1.reviews.create!(
+ title: "Small store front",
+ rating: 2,
+ content: "3n2 ww q2 ewlrjwe ke wrkw qw jw nwke 23 2nqln qn onrkew 3k iw dqqm",
+ image_path: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHrpmj6jvqKicV9ttIunW_Oz_PM-x5RfLsPYYCdAlw6uLf628JxA&s")
