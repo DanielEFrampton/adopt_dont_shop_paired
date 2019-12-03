fb5035aece58d80895edf3a3160195189ff2eb28
Ryan Allen <rallenwasd@gmail.com>
2019-12-03 07:42:10 -0700

    Add conditional to redirect on save or error

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,8 +5,13 @@ class ReviewsController < ApplicationController
 
   def create
     @shelter = Shelter.find(params[:id])
-    review = @shelter.reviews.create(review_params)
-    redirect_to "/shelters/#{@shelter.id}"
+    review = @shelter.reviews.new(review_params)
+    if review.save
+      redirect_to "/shelters/#{@shelter.id}"
+    else
+      flash[:notice] = "Not all required fields have input"
+      redirect_to "/shelters/#{@shelter.id}/reviews/new"
+    end
   end
 
   private
