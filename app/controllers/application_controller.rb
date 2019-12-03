bae57767fe036ae25e39320287fdd0658aa46368
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-03 15:53:06 -0700

    Add method to setup favorites variables to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :setup_favorites
+
+  private
+
+    def setup_favorites
+      @favorites = cookies[:favorites] ||= []
+      @num_favorites = @favorites.length
+    end
 end
