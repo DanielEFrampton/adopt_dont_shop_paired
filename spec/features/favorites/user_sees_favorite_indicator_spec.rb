d50fbe0fb0d3e24484aa74a71a6b1542dfb48bdd
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-03 11:57:24 -0700

    Add feature test for top navbar favorites indicator

---
+++ spec/features/favorites/user_sees_favorite_indicator_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor', type: :feature do
+  it 'I see a favorite indicator in my navigation bar on any page with count of pets in my favorites list' do
+    visit '/'
+
+    within('#site-header') do
+      expect(page).to have_content('Favorites (0)')
+    end
+
+    visit '/pets'
+
+    within('#site-header') do
+      expect(page).to have_content('Favorites (0)')
+    end
+
+    visit '/pets/new'
+
+    within('#site-header') do
+      expect(page).to have_content('Favorites (0)')
+    end
+
+    visit '/shelters'
+
+    within('#site-header') do
+      expect(page).to have_content('Favorites (0)')
+    end
+
+    visit '/shelters/new'
+
+    within('#site-header') do
+      expect(page).to have_content('Favorites (0)')
+    end
+  end
+end
