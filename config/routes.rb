f70fa32427e5ab6646dfede1193bac2083504d3d
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-03 23:10:33 -0700

    Add route to destroy all favorited pets

--- config/routes.rb
+++ config/routes.rb
@@ -24,4 +24,5 @@ Rails.application.routes.draw do
   post 'pets/:id/favorites', to: 'favorites#create'
   get '/favorites', to: 'favorites#index'
   delete '/favorites/:id', to: 'favorites#destroy'
+  delete '/favorites', to: 'favorites#destroy_all'
 end
