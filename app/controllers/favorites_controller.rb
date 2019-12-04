d4ecd06751c133bd5e39a7d01e01ef2cb42fdddb
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-03 23:14:30 -0700

    Add action to favorites controller to destroy all favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -16,4 +16,9 @@ class FavoritesController < ApplicationController
     flash[:unfavorited] = "This pet has been removed from your favorites. :("
     redirect_back(fallback_location: '/favorites')
   end
+
+  def destroy_all
+    cookies[:favorites] = []
+    redirect_to '/favorites'
+  end
 end
