3e4ed317443ac57ea858dce20a949d0fb08df70a
Ryan Allen <rallenwasd@gmail.com>
2019-12-03 18:16:29 -0700

    Add reviews update route

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
   patch '/pets/:id/:new_status', to:'pets#update_status'
 
   get '/shelters/:id/reviews/new', to: 'reviews#new'
-  get '/reviews/:review_id/edit', to: 'reviews#edit'
+  get '/reviews/:id/edit', to: 'reviews#edit'
   post '/shelters/:id', to: 'reviews#create'
+  patch '/reviews/:id', to: 'reviews#update'
 end
