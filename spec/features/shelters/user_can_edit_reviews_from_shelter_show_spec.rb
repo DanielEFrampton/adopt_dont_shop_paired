4b8fc746b46955b8c7b2d10e5d91f064084b80a4
Ryan Allen <rallenwasd@gmail.com>
2019-12-03 18:15:55 -0700

    Add test that user can make a change to a review and then see those changes

--- spec/features/shelters/user_can_edit_reviews_from_shelter_show_spec.rb
+++ spec/features/shelters/user_can_edit_reviews_from_shelter_show_spec.rb
@@ -3,25 +3,25 @@ require 'rails_helper'
 describe "As a visitor" do
   describe "When I visit a shelter's show page" do
     before :each do
-      shelter = Shelter.create!(
+      @shelter = Shelter.create!(
         name: "Ridiculous Test Name",
         address: "124 Fake Ln.",
         city: "Faketown",
         state: "FK",
         zip: "55555")
 
-      @review_1 = shelter.reviews.create!(
+      @review_1 = @shelter.reviews.create!(
         title: "Great Location",
         rating: 4,
         content: "3n22 ewlrjwe kej wrkjw nwke nqln qn onrkew 3k iw qm",
         image_path: "https://www.shutterstock.com/image-vector/location-pin-700686334")
 
-      @review_2 = shelter.reviews.create!(
+      @review_2 = @shelter.reviews.create!(
         title: "Small store front",
         rating: 2,
         content: "3n2 ww q2 ewlrjwe ke wrkw qw jw nwke 23 2nqln qn onrkew 3k iw dqqm")
 
-      visit("/shelters/#{shelter.id}")
+      visit("/shelters/#{@shelter.id}")
     end
 
     it "I see a link to edit the shelter review next to each review." do
@@ -49,6 +49,18 @@ describe "As a visitor" do
         expect(find_field('Content').value).to eq(@review_1.content)
         expect(find_field('Image Path').value).to eq(@review_1.image_path)
 
+      end
+
+      it "I can update any of these fields, sumbit the form, and see my updated review on shelter's show page" do
+        visit("/reviews/#{@review_1.id}/edit")
+
+        fill_in 'Title', with: "Maybe Closed?"
+        fill_in 'Rating', with: 3
+        fill_in 'Content', with: "Drove by two days in a row, closed sign was up."
+        fill_in 'Image Path', with: "https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+
+        click_button 'Submit'
+        expect(current_path).to eq("/shelters/#{@review_1.shelter.id}")
       end
     end
   end
