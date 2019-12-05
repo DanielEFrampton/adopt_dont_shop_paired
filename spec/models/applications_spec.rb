28bc5b91291d4370c6941de1ff3e10bf96261303
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-05 14:39:53 -0700

    Add relationships to application model test and model
    Co-Authored-By: null <rallen20@users.noreply.github.com>

--- spec/models/applications_spec.rb
+++ spec/models/applications_spec.rb
@@ -9,4 +9,9 @@ describe Application, type: :model do
     it {should validate_presence_of :zip}
     it {should validate_presence_of :description}
   end
+
+  describe "relationships" do
+    it {should have_many :pet_applications}
+    it {should have_many(:pets).through(:pet_applications)}
+  end
 end
