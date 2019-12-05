d9c8d22fb5767b57cdf56e9ccf8f18ab4ea08c09
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-05 14:03:34 -0700

    Change initialization of favorites list to expect hash

--- app/models/favorites_list.rb
+++ app/models/favorites_list.rb
@@ -1,8 +1,10 @@
 class FavoritesList
   attr_reader :ids
 
-  def initialize(pet_ids)
-    @ids = pet_ids.split(',') || []
+  # anticipating that favorited_pets comes in as a hash from the session;
+  # i.e., session[:favorited_pets] = {"341" => true; "457" => true}
+  def initialize(favorited_pets)
+    @favorited_pets = favorited_pets
   end
 
   def total_favorites
