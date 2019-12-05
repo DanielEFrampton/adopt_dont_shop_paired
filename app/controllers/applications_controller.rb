26da195002cd6ff6491182522d42300789bc6be9
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-05 13:56:48 -0700

    Add helper methods and sad path break if nothing checked
    Co-Authored-By: null <rallen20@users.noreply.github.com>

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,6 +4,7 @@ class ApplicationsController < ApplicationController
   end
 
   def create
+    return nothing_checked if !params.has_key?(:favorite_ids)
     new_application = Application.new(application_params)
     if new_application.save
       favorites = cookies[:favorites].split(',')
@@ -18,8 +19,18 @@ class ApplicationsController < ApplicationController
       flash[:applied] = "Your application for the selected pets has been submitted."
       redirect_to "/favorites"
     else
-      flash.now[:incomplete] = "You must complete all fields in order to submit the application."
-      render :new
+      flash[:incomplete] = "You must complete all fields in order to submit the application."
+      redirect_to '/applications/new'
     end
   end
+
+  private
+    def application_params
+      params.permit(:name, :address, :city, :state, :zip, :phone_number, :description)
+    end
+
+    def nothing_checked
+      flash[:nothing_checked] = "You must select at least one favorited pet by checking its box."
+      redirect_to '/applications/new'
+    end
 end
