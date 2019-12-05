c1c072f6989b286ec0e13a85cc4870bc4f4cffeb
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-05 13:02:53 -0700

    Add conditional paths to create action on applications controller
    Co-Authored-By: null <rallen20@users.noreply.github.com>

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,16 +4,22 @@ class ApplicationsController < ApplicationController
   end
 
   def create
-    favorites = cookies[:favorites].split(',')
+    new_application = Application.new(application_params)
+    if new_application.save
+      favorites = cookies[:favorites].split(',')
 
-    # possible refactor to helper method
-    params[:favorite_ids].each do |favorite_id|
-      favorites.delete(favorite_id)
+      # possible refactor to helper method
+      params[:favorite_ids].each do |favorite_id|
+        favorites.delete(favorite_id)
+      end
+
+      cookies[:favorites] = favorites.join(',')
+
+      flash[:applied] = "Your application for the selected pets has been submitted."
+      redirect_to "/favorites"
+    else
+      flash.now[:incomplete] = "You must complete all fields in order to submit the application."
+      render :new
     end
-
-    cookies[:favorites] = favorites.join(',')
-
-    flash[:applied] = "Your application for the selected pets has been submitted."
-    redirect_to "/favorites"
   end
 end
