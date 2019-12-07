cea630e0df8ed6e5ec32371c64d99c49ecc1576f
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-07 10:49:46 -0700

    Move favorites list modification code to PORO from application create

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,14 +4,8 @@ class ApplicationsController < ApplicationController
   end
 
   def create
-    favorites = cookies[:favorites].split(',')
-
-    # possible refactor to helper method
-    params[:favorite_ids].each do |favorite_id|
-      favorites.delete(favorite_id)
-    end
-
-    cookies[:favorites] = favorites.join(',')
+    @favorites.delete(params[:favorite_ids])
+    session[:favorites] = @favorites.favorited_pets
 
     flash[:applied] = "Your application for the selected pets has been submitted."
     redirect_to "/favorites"
