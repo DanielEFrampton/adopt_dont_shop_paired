0d9d533dc5b21634b572a19ebe6028f6a8adb418
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-07 09:27:13 -0700

    Refactor favorites list to work with initial hash argument
    Co-Authored-By: null <rallen20@users.noreply.github.com>

--- app/models/favorites_list.rb
+++ app/models/favorites_list.rb
@@ -1,13 +1,17 @@
 class FavoritesList
-  attr_reader :ids
+  attr_reader :favorited_pets
 
   # anticipating that favorited_pets comes in as a hash from the session;
   # i.e., session[:favorited_pets] = {"341" => true; "457" => true}
-  def initialize(favorited_pets)
+  def initialize(favorited_pets = Hash.new)
     @favorited_pets = favorited_pets
   end
 
+  def ids
+    @favorited_pets.keys
+  end
+
   def total_favorites
-    @ids.length
+    @favorited_pets.keys.count
   end
 end
