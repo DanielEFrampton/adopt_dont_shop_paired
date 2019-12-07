a32b2e6b8225de6af91b0472942fbf7591fbc4c8
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-07 09:26:59 -0700

    Revise and add expectations of favorites list model test
    Co-Authored-By: null <rallen20@users.noreply.github.com>

--- spec/models/favorites_list_spec.rb
+++ spec/models/favorites_list_spec.rb
@@ -1,25 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe FavoritesList, type: :model do
+before(:each) do
+  @favorites_hash = {"1" => true,
+                    "2" => true,
+                    "3" => true,
+                    "4" => true}
+  @list_1 = FavoritesList.new(@favorites_hash)
+  @list_2 = FavoritesList.new
+end
+
   describe "methods" do
-    it "can initialize with attribute containing array of favorited pet ids" do
-      list_1 = FavoritesList.new("1,2,3,4")
+    it "can initialize with attribute containing hash of favorited pet ids and booleans" do
+      expect(@list_1.favorited_pets).to eq(@favorites_hash)
+    end
 
-      expect(list_1.ids).to eq(['1', '2', '3', '4'])
+    it "can initialize without arguments with default empty hash value" do
+      expect(@list_2.favorited_pets).to eq({})
+    end
 
-      list_2 = FavoritesList.new("")
-
-      expect(list_2.ids).to eq([])
+    it 'can return array of ids of favorited pets' do
+      expect(@list_1.ids).to eq(%w(1 2 3 4))
     end
 
     it "it can return total number of favorited pets" do
-      list_1 = FavoritesList.new("1,2,3,4")
-
-      expect(list_1.total_favorites).to eq(4)
-
-      list_2 = FavoritesList.new("")
-
-      expect(list_2.total_favorites).to eq(0)
+      expect(@list_1.total_favorites).to eq(4)
+      expect(@list_2.total_favorites).to eq(0)
     end
   end
 end
