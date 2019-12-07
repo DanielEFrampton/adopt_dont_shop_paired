616a817065c95f2b52c9ca7d405226731210bd3a
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-07 10:50:03 -0700

    Update favorites controller to rely on PORO for deletion

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,21 +4,21 @@ class FavoritesController < ApplicationController
   end
 
   def create
-    cookies[:favorites] += "#{!cookies[:favorites].empty? ? ',' : ''}#{params[:id]}"
+    @favorites.add(params[:id])
+    session[:favorites] = @favorites.favorited_pets
     flash[:favorited] = "This pet has been added to your favorites! :D"
     redirect_to "/pets/#{params[:id]}"
   end
 
   def destroy
-    favorites = cookies[:favorites].split(',')
-    favorites.delete(params[:id])
-    cookies[:favorites] = favorites.join(',')
+    @favorites.delete(params[:id])
     flash[:unfavorited] = "This pet has been removed from your favorites. :("
     redirect_back(fallback_location: '/favorites')
   end
 
   def destroy_all
-    cookies[:favorites] = ""
+    @favorites.delete_all
+    session[:favorites] = @favorites.favorited_pets
     redirect_to '/favorites'
   end
 end
