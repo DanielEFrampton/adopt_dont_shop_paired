02ecbf5e8c733f4812533e0820956a54087aa3d2
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-07 11:03:23 -0700

    Add flash message when deleting all favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -12,6 +12,7 @@ class FavoritesController < ApplicationController
 
   def destroy
     @favorites.delete(params[:id])
+    session[:favorites] = @favorites.favorited_pets
     flash[:unfavorited] = "This pet has been removed from your favorites. :("
     redirect_back(fallback_location: '/favorites')
   end
@@ -19,6 +20,7 @@ class FavoritesController < ApplicationController
   def destroy_all
     @favorites.delete_all
     session[:favorites] = @favorites.favorited_pets
+    flash[:unfavorited_all] = "You have removed all pets from your favorites list. ╥﹏╥"
     redirect_to '/favorites'
   end
 end
