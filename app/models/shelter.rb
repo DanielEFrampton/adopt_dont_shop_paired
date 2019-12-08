5a24acb790b0c50ac9d4ef2b283e13a7cffe5a6e
Ryan Allen <rallenwasd@gmail.com>
2019-12-08 07:35:34 -0700

    Add model method no pending and user story 26 feature tests pass

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -6,4 +6,12 @@ class Shelter < ApplicationRecord
   def pet_count
     pets.count
   end
+
+  def no_pending? # needs heavy refactor, ruby doc site is down (~12/8/19 7am)
+    return true if pet_count == 0
+    pets.each do |pet|
+      return false if !pet.adoptable
+    end
+    true
+  end
 end
