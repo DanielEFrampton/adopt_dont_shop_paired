f2606d57112e9c7a5d08e76b0826b7a3f92fa3c0
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-09 15:21:52 -0700

    Change relationship between pet and joins table to cause cascade delete

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,7 +2,7 @@ class Pet < ApplicationRecord
   validates_presence_of :name, :sex, :approx_age, :image_path, :description
   validates_inclusion_of :adoptable, :in => [true, false]
   belongs_to :shelter
-  has_many :pet_applications
+  has_many :pet_applications, dependent: :destroy
   has_many :applications, through: :pet_applications
 
   def adoptable_status
