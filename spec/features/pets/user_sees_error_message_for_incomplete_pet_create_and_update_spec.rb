0e502e30c9ef94b9af7ee0e2e6b9809b2bdb97a4
Ryan Allen <rallenwasd@gmail.com>
2019-12-09 14:37:17 -0700

    Add additional test coverage for user story 33

--- spec/features/pets/user_sees_error_message_for_incomplete_pet_create_and_update_spec.rb
+++ spec/features/pets/user_sees_error_message_for_incomplete_pet_create_and_update_spec.rb
@@ -9,25 +9,48 @@ describe "As a visitor" do
                                  state: "FK",
                                  zip: "55555")
   end
-  describe "When I am updating or creating a pet, If I try to submit the form with incomplete information" do
+  describe "When I am creating a pet, If I try to submit the form with incomplete information" do
     it "I see a flash message indicating all field(s)" do
       visit "/shelters/#{@shelter_1.id}/pets/new"
       click_button 'Submit'
-      expect(page).to have_content("You attempted to submit the form without completing required field(s): Name, Sex, Approximate Age, Description, Image Path")
+      expect(page).to have_content("You attempted to submit the form without completing required field(s): Name, Approx_age, Sex, Image_path, Description")
     end
 
-    # it "I see a flash message indicating which field(s) I am missing" do
-    #   visit "/shelters/#{@shelter_1.id}/pets/new"
-    #
-    #   fill_in 'Name', with: "Phil"
-    #   # fill_in 'Sex', with: "male"
-    #   fill_in 'Approximate Age', with: "1"
-    #   # fill_in 'Description', with: "Very cute"
-    #   fill_in 'Image Path', with: "http://files.alfresco.mjh.group/alfresco_images/DVM360//2019/02/27/c23624de-1b4c-4e61-961f-db10dcf27341/veterinary-black-puppy-licking-wood-AdobeStock_211167675-450.jpg"
-    #
-    #   click_button 'Submit'
-    #   expect(page).to have_content("You attempted to submit the form without completing required field(s): Sex, Description")
-    #
-    # end
+    it "I see a flash message indicating which field(s) I am missing" do
+      visit "/shelters/#{@shelter_1.id}/pets/new"
+
+      fill_in 'Name', with: "Phil"
+      # fill_in 'Sex', with: "male"
+      fill_in 'Approximate Age', with: "1"
+      # fill_in 'Description', with: "Very cute"
+      fill_in 'Image Path', with: "http://files.alfresco.mjh.group/alfresco_images/DVM360//2019/02/27/c23624de-1b4c-4e61-961f-db10dcf27341/veterinary-black-puppy-licking-wood-AdobeStock_211167675-450.jpg"
+
+      click_button 'Submit'
+      expect(page).to have_content("You attempted to submit the form without completing required field(s): Sex, Description")
+    end
+  end
+
+  describe "When I am updating a pet, If I try to submit the form with incomplete information" do
+    it "I see a flash message indiciating which field(s) I am missing" do
+      @pet_1 = @shelter_1.pets.create({
+                                       name: "Bill",
+                                       approx_age: 3,
+                                       sex: "male",
+                                       image_path: "https://www.petmd.com/sites/default/files/Acute-Dog-Diarrhea-47066074.jpg",
+                                       description: "Very canine",
+                                       adoptable: true
+                                     })
+      visit "/pets/#{@pet_1.id}/edit"
+
+      fill_in 'Name', with: ''
+      fill_in 'Approximate Age', with: 67
+      fill_in 'Sex', with: 'female'
+      fill_in 'Description', with: ''
+      fill_in 'Image Path', with: "https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_1280.jpg"
+
+      click_button 'Submit'
+
+      expect(page).to have_content("You attempted to submit the form without completing required field(s): Name, Description")
+    end
   end
 end
