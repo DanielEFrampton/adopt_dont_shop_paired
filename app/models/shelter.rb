6ce4b5d12d7306636b8e29a49f7fa22decec8d64
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-11 12:28:18 -0700

    Add top_rated method to Shelter model

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -18,4 +18,12 @@ class Shelter < ApplicationRecord
   def application_count
     Shelter.where(id: id).joins(pets: :applications).distinct.count
   end
+
+  def self.top_rated
+    select('shelters.name, shelters.id, AVG(reviews.rating) AS average_rating')
+    .joins(:reviews)
+    .order('average_rating DESC')
+    .limit(3)
+    .group('shelters.id')
+  end
 end
