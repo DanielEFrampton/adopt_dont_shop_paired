705e112afc8ca0b0ef801ace954416f268fc4eda
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-11 13:06:02 -0700

    Adjust test data to detect edge cases

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -175,7 +175,7 @@ describe Shelter, type: :model do
                                sex: "male",
                                image_path: "https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_1280.jpg",
                                description: "Very canine",
-                               adoptable: true
+                               adoptable: false
                              })
       @pet_2 = @shelter_1.pets.create({
                                name: "Phil",
@@ -191,7 +191,7 @@ describe Shelter, type: :model do
                                sex: "male",
                                image_path: "https://boygeniusreport.files.wordpress.com/2016/11/puppy-dog.jpg",
                                description: "Very canine",
-                               adoptable: true
+                               adoptable: false
                              })
 
       @pet_4 = @shelter_2.pets.create({
@@ -200,7 +200,7 @@ describe Shelter, type: :model do
                                sex: "male",
                                image_path: "https://boygeniusreport.files.wordpress.com/2016/11/puppy-dog.jpg",
                                description: "Very canine",
-                               adoptable: true
+                               adoptable: false
                              })
       @pet_5 = @shelter_2.pets.create({
                                name: "Philippa",
@@ -208,7 +208,7 @@ describe Shelter, type: :model do
                                sex: "male",
                                image_path: "https://boygeniusreport.files.wordpress.com/2016/11/puppy-dog.jpg",
                                description: "Very canine",
-                               adoptable: true
+                               adoptable: false
                              })
 
       @pet_6 = @shelter_3.pets.create({
